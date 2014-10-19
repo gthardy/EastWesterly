d5ce5f7651874489480b8d88fa0ee4b31bc999c0
gthardy <gthardy@averware.com>
2014-10-19 07:07:45 -0400

    added nodemailer

--- public/controllers/contact-controller.js
+++ public/controllers/contact-controller.js
@@ -1,3 +1,13 @@
 app.controller('contactController', function($scope){
 	$scope.Title = "Contact";
+
+    $scope.Name = "Jackson Hardy";
+    $scope.Email = "jackson.hardy@averware.com";
+    $scope.Phone = "999-999-9999";
+    $scope.BestContactTime = "Afternoon 5PM";
+
+    $scope.SendEmail = function(){
+        toastr.success("Email has been sent!", "you will be hearing form us shortly!");
+    };
+
 });
